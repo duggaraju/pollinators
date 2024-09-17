1d0d45949a26f8803ae6fd23f285dd8adbb8073b
Prakash Duggaraju <duggaraju@gmail.com>
2024-09-16 22:21:14 -0700

    Use tailwind
    Update the CSS for tailwind.

--- Pollinators/pollinators.client/src/App.tsx
+++ Pollinators/pollinators.client/src/App.tsx
@@ -1,33 +1,40 @@
-import "./App.css";
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import Form from "./pages/Form";
 import Dashboard from "./pages/Dashboard";
 
-
 function App() {
   return (
-    <div>
-      <h1 id="tableLabel">People + Pollinators</h1>
-
-    <Router>
-        <nav>
-            <ul>
-                <li>
-                    <Link to="/dashboard">Dashboard</Link>
-                </li>
-                <li>
-                    <Link to="/report">Report</Link>
-                </li>
-            </ul>
-        </nav>
+    <div className="bg-white dark:bg-black">
+        <h1 className="text-3xl">People + Pollinators</h1>
+        <Router>
+        <ul
+          className="flex mb-0 list-none flex-wrap pt-3 pb-4 flex-row"
+          role="tablist"
+        >
+          <li className="-mb-px mr-2 last:mr-0 flex-auto text-center">
+          </li>
+          <li className="-mb-px mr-2 last:mr-0 flex-auto text-center">
+            <Link to="/dashboard">
+              <button className="bg-blue-500 px-4 text-white ml-4 rounded-full">
+                Dashboard
+              </button>
+            </Link>
+          </li>
+          <li className="-mb-px mr-2 last:mr-0 flex-auto text-center">
+            <Link to="/report">
+              <button className="bg-blue-500 px-4 text-white ml-4 rounded-full">
+                Report
+              </button>
+            </Link>
+          </li>
+        </ul>
         <Routes>
-            <Route path="/dashboard" element={<Dashboard/>} />
-            <Route path="/report" element={
-                <Form />
-            } />
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/report" element={<Form />} />
         </Routes>
-    </Router>
-        </div>
+      </Router>
+    </div>
   );
 }
 
--- Pollinators/pollinators.client/src/pages/Dashboard.tsx
+++ Pollinators/pollinators.client/src/pages/Dashboard.tsx
@@ -3,9 +3,8 @@ import React from 'react';
 
 const Dashboard: React.FC = () => {
   return (
-	<div>
-	  <h1>Map Display</h1>
-	  <div id="map" style={{ width: '100%', height: '500px', backgroundColor: '#eaeaea' }}>
+	<div className="min-h-fit">
+	  <div id="map" style={{ width: '100%', backgroundColor: '#eaeaea' }}>
 		{/* Map will be displayed here */}
 	  </div>
 	</div>
